95d249009c0dad125c0eebb6f69e31ff550d345f
Adam Jonas <adamjonas25@gmail.com>
2023-12-21 21:27:49 +0000

    Initial commit

---
+++ util/src/test/kotlin/com/github/aj8gh/aok/util/ReaderTest.kt
@@ -0,0 +1,12 @@
+package com.github.aj8gh.aok.util
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ReaderTest {
+
+  @Test
+  fun read() {
+    assertTrue(true)
+  }
+}
---
+++ y-23/src/test/kotlin/com/github/aj8gh/aok/y23/ReaderTest.kt
@@ -0,0 +1,12 @@
+package com.github.aj8gh.aok.y23
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ReaderTest {
+
+  @Test
+  fun read() {
+    assertTrue(true)
+  }
+}
