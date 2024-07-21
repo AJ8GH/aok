c28fdda1b738a18f1a6b4b750c3e6e8eaa2d1daf
Adam Jonas <adamjonas25@gmail.com>
2024-07-21 11:20:29 +0100

    Add aoc files

---
+++ y-15/src/main/kotlin/com/github/aj8gh/aok/y15/d1/Day1.kt
@@ -0,0 +1,5 @@
+package com.github.aj8gh.aok.y15.d1
+
+fun part1(input: List<String>) = 74
+
+fun part2(input: List<String>) = 1795
---
+++ y-16/src/main/kotlin/com/github/aj8gh/aok/y16/d2/Day2.kt
@@ -0,0 +1,5 @@
+package com.github.aj8gh.aok.y16.d2
+
+fun part1(input: List<String>) = 0
+
+fun part2(input: List<String>) = 0
