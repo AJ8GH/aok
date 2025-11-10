6a797e1551cf72a46fe53c4d7633a6fe33fbe20d
Adam Jonas <adamjonas25@gmail.com>
2025-11-10 21:00:50 +0000

    Solve d9 l1

--- y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d9/Day9.kt
+++ y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d9/Day9.kt
@@ -1,5 +1,7 @@
 package io.github.aj8gh.aok.y24.d9
 
+import java.util.ArrayDeque
+
 data class File(var id: Int, var size: Int)
 
 fun part1(input: List<String>) = solve(input.first())
@@ -7,49 +9,48 @@ fun part1(input: List<String>) = solve(input.first())
 fun part2(input: List<String>) = 0L
 
 private fun solve(input: String): Long {
-  var total = 0L
+  var formatted = ""
+  val files = ArrayDeque<File>()
+  val spaces = ArrayDeque<File>()
+  for (i in input.indices) {
+    if (i % 2 == 0) {
+      files.add(File(i / 2, input[i].digitToInt()))
+    } else {
+      spaces.add(File(-1, input[i].digitToInt()))
+    }
+  }
 
   var i = 0
-  var finalIndex = 0
-  var j = input.length - 1
-  var endFile = File(id = j / 2, size = input[j].digitToInt())
-  val processedI = mutableSetOf<Int>()
-  val processedJ = mutableSetOf<Int>()
-  var reformated = ""
-
-  while (i < input.length && j > 0) {
+  var index = 0
+  var total = 0L
+  while (files.isNotEmpty()) {
     if (i % 2 == 0) {
-      if (processedJ.contains(i)) {
-        i++
-        continue
+      val fileFromBottom = files.removeFirst()
+      repeat(fileFromBottom.size) {
+        total += fileFromBottom.id * index
+        index++
+        formatted += fileFromBottom.id
       }
-      val startFile = File(id = i / 2, size = input[i].digitToInt())
-      repeat(startFile.size) {
-        reformated += startFile.id
-        total += startFile.id * finalIndex++
-      }
-      processedI.add(i)
     } else {
-      if (processedI.contains(j)) {
-        i++
-        continue
-      }
-      processedJ.add(j)
-      var space = input[i].digitToInt()
-      end@ while (space-- > 0) {
-        if (endFile.size <= 0) {
-          j -= 2
-          if (processedI.contains(j)) {
-            break@end
+      val space = spaces.removeFirst()
+      var fileFromTop = files.last
+      while (space.size > 0) {
+        if (fileFromTop.size <= 0) {
+          if (files.size == 1) {
+            break
           }
-          endFile = File(id = j / 2, size = input[j].digitToInt())
+          files.removeLast()
+          fileFromTop = files.last
         }
-        reformated += endFile.id
-        total += endFile.id * finalIndex++
-        endFile.size--
+        total += fileFromTop.id * index
+        fileFromTop.size--
+        space.size--
+        index++
+        formatted += fileFromTop.id
       }
     }
     i++
   }
+
   return total
 }
