8ecc0c08f968e5af0647bdec26658b2bb9ab6fea
Adam Jonas <adamjonas25@gmail.com>
2025-11-19 13:12:59 +0000

    Solve d16 l1

--- y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d16/Day16.kt
+++ y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d16/Day16.kt
@@ -1,5 +1,100 @@
 package io.github.aj8gh.aok.y24.d16
 
-fun part1(input: List<String>) = 0
+import kotlin.Int.Companion.MAX_VALUE
+
+/*
+  keep map of position visited and score
+  bfs of each possible path
+  abandon path if position is visited and saved score is lower than current
+  if current score is lower, update in visited map
+  return score for end position in map
+*/
+
+const val START = 'S'
+const val END = 'E'
+const val WALL = '#'
+
+const val TURN_COST = 1000
+const val MOVE_COST = 1
+
+const val NO_TURN = 0
+const val CLOCKWISE_TURN = 90
+const val ONE_EIGHTY_TURN = 180
+const val ANTI_CLOCKWISE_TURN = 270
+
+const val EAST = '>'
+const val SOUTH = 'v'
+const val WEST = '<'
+const val NORTH = '^'
+
+val TURNS = listOf(NO_TURN, CLOCKWISE_TURN, ANTI_CLOCKWISE_TURN, ONE_EIGHTY_TURN)
+val DIRS = listOf(EAST, SOUTH, WEST, NORTH)
+
+data class Route(
+  val point: Pair<Int, Int>,
+  val dir: Char,
+  val score: Int,
+)
+
+fun part1(input: List<String>): Int {
+  var start = Pair(-1, -1)
+  var end = Pair(-1, -1)
+  for (i in input.indices) {
+    for (j in input[i].indices) {
+      when (input[i][j]) {
+        START -> start = Pair(i, j)
+        END -> end = Pair(i, j)
+      }
+    }
+  }
+  return findShortestPath(input, start, end)
+}
 
 fun part2(input: List<String>) = 0
+
+private fun findShortestPath(
+  input: List<String>,
+  start: Pair<Int, Int>,
+  end: Pair<Int, Int>,
+): Int {
+  val visited = mutableMapOf<Pair<Int, Int>, Int>()
+  val routes = ArrayDeque<Route>()
+  routes.add(Route(start, EAST, 0))
+  while (routes.isNotEmpty()) {
+    val route = routes.removeLast()
+    if ((visited[route.point] ?: MAX_VALUE) > route.score) {
+      visited[route.point] = route.score
+      addPossibleRoutes(input, routes, route)
+    }
+  }
+  return visited[end]!!
+}
+
+private fun addPossibleRoutes(
+  input: List<String>,
+  routes: ArrayDeque<Route>,
+  route: Route,
+) {
+  if (get(input, route) != END) {
+    TURNS.map { next(route, it) }
+      .filter { get(input, it) != WALL }
+      .forEach { routes.add(it) }
+  }
+}
+
+private fun next(route: Route, turn: Int): Route {
+  val turnScore = (if (turn == ANTI_CLOCKWISE_TURN) CLOCKWISE_TURN else turn) / CLOCKWISE_TURN
+  val score = route.score + turnScore * TURN_COST + MOVE_COST
+  val dir = DIRS[(turn / CLOCKWISE_TURN + DIRS.indexOf(route.dir)) % DIRS.size]
+  val point = when (dir) {
+    EAST -> Pair(route.point.first, route.point.second + 1)
+    SOUTH -> Pair(route.point.first + 1, route.point.second)
+    WEST -> Pair(route.point.first, route.point.second - 1)
+    NORTH -> Pair(route.point.first - 1, route.point.second)
+    else -> throw IllegalArgumentException("Bad dir $dir")
+  }
+  return Route(point, dir, score)
+}
+
+private fun get(input: List<String>, route: Route) =
+  input[route.point.first][route.point.second]
--- y-24/src/test/kotlin/io/github/aj8gh/aok/y24/d16/Day16Test.kt
+++ y-24/src/test/kotlin/io/github/aj8gh/aok/y24/d16/Day16Test.kt
@@ -1,6 +1,7 @@
 package io.github.aj8gh.aok.y24.d16
 
 import io.github.aj8gh.aok.util.DAY_16
+import io.github.aj8gh.aok.util.EXAMPLE_1
 import io.github.aj8gh.aok.util.INPUT_PROVIDER_PART_1
 import io.github.aj8gh.aok.util.INPUT_PROVIDER_PART_2
 import io.github.aj8gh.aok.util.InputReader.Reader.readExample
@@ -25,13 +26,14 @@ class Day16Test {
   companion object {
 
     @JvmStatic
-    private fun inputProviderPart1() = getInput(0, 0)
+    private fun inputProviderPart1() = getInput(7036, 11048, 109516)
 
     @JvmStatic
-    private fun inputProviderPart2() = getInput(0, 0)
+    private fun inputProviderPart2() = getInput(45, 64, 0)
 
-    private fun getInput(example: Int, answer: Int) = listOf(
+    private fun getInput(example: Int, example1: Int, answer: Int) = listOf(
       Arguments.of(readExample(DAY_16), example),
+      Arguments.of(readExample(DAY_16, EXAMPLE_1), example1),
       Arguments.of(readInput(DAY_16), answer),
     )
   }
