fb8074b406561c9a5e46bb85e4a9c5d86c2cb89c
Adam Jonas <adamjonas25@gmail.com>
2025-11-20 22:06:52 +0000

    Start d17 l1

--- y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d17/Day17.kt
+++ y-24/src/main/kotlin/io/github/aj8gh/aok/y24/d17/Day17.kt
@@ -1,5 +1,56 @@
 package io.github.aj8gh.aok.y24.d17
 
-fun part1(input: List<String>) = 0
+import kotlin.math.pow
 
-fun part2(input: List<String>) = 0
+data class Registers(
+  var a: Int,
+  var b: Int,
+  var c: Int,
+)
+
+val digits = Regex("\\d+")
+const val TWO = 2.0
+
+fun part1(input: List<String>): String {
+  val nums = input.map { digits.findAll(it).map { it.value.toInt() }.toList() }
+    .filter { it.isNotEmpty() }
+  val reg = nums.slice(0..2).flatten().let { Registers(it[0], it[1], it[2]) }
+  val program = nums.last()
+  val out = mutableListOf<Int>()
+
+  var i = 0
+  while (i < program.lastIndex) {
+    val opcode = program[i]
+    val operand = program[i + 1]
+    when (opcode) {
+      0 -> reg.a /= TWO.pow(combo(reg, operand).toDouble()).toInt()
+      1 -> reg.b = reg.b xor operand
+      2 -> reg.b = combo(reg, operand) % 8
+      3 -> {
+        if (reg.a != 0) {
+          i = operand / 2
+          continue
+        }
+      }
+
+      4 -> reg.b = reg.b xor reg.c
+      5 -> out.add(combo(reg, operand) % 8)
+      6 -> reg.b /= TWO.pow(combo(reg, operand).toDouble()).toInt()
+      7 -> reg.c /= TWO.pow(combo(reg, operand).toDouble()).toInt()
+    }
+    i += 2
+  }
+  return out.joinToString(",")
+}
+
+fun part2(input: List<String>): String = TODO()
+
+private fun combo(
+  registers: Registers,
+  operand: Int
+) = when (operand) {
+  4 -> registers.a
+  5 -> registers.b
+  6 -> registers.c
+  else -> operand
+}
--- y-24/src/test/kotlin/io/github/aj8gh/aok/y24/d17/Day17Test.kt
+++ y-24/src/test/kotlin/io/github/aj8gh/aok/y24/d17/Day17Test.kt
@@ -1,6 +1,9 @@
 package io.github.aj8gh.aok.y24.d17
 
 import io.github.aj8gh.aok.util.DAY_17
+import io.github.aj8gh.aok.util.EXAMPLE_1
+import io.github.aj8gh.aok.util.EXAMPLE_2
+import io.github.aj8gh.aok.util.EXAMPLE_3
 import io.github.aj8gh.aok.util.INPUT_PROVIDER_PART_1
 import io.github.aj8gh.aok.util.INPUT_PROVIDER_PART_2
 import io.github.aj8gh.aok.util.InputReader.Reader.readExample
@@ -14,23 +17,36 @@ class Day17Test {
 
   @ParameterizedTest
   @MethodSource(INPUT_PROVIDER_PART_1)
-  fun part1(input: List<String>, expected: Int) =
+  fun part1(input: List<String>, expected: String) =
     assertEquals(expected, part1(input))
 
   @ParameterizedTest
   @MethodSource(INPUT_PROVIDER_PART_2)
-  fun part2(input: List<String>, expected: Int) =
+  fun part2(input: List<String>, expected: String) =
     assertEquals(expected, part2(input))
 
   companion object {
 
     @JvmStatic
-    private fun inputProviderPart1() = getInput(0, 0)
+    private fun inputProviderPart1() = getInput(
+      "0,1,2",
+      "4,2,5,6,7,7,7,7,3,1,0",
+      "4,6,3,5,6,3,5,2,1,0",
+      ""
+    )
 
     @JvmStatic
-    private fun inputProviderPart2() = getInput(0, 0)
+    private fun inputProviderPart2() = getInput("", "", "", "")
 
-    private fun getInput(example: Int, answer: Int) = listOf(
+    private fun getInput(
+      example1: String,
+      example2: String,
+      example: String,
+      answer: String
+    ) = listOf(
+//      Arguments.of(readExample(DAY_17, EXAMPLE_3), example1),
+      Arguments.of(readExample(DAY_17, EXAMPLE_1), example1),
+      Arguments.of(readExample(DAY_17, EXAMPLE_2), example2),
       Arguments.of(readExample(DAY_17), example),
       Arguments.of(readInput(DAY_17), answer),
     )
