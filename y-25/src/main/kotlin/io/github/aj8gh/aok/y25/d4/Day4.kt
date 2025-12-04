93bfbf79e4397158b432152a9e57d8764c48177f
Adam Jonas <adamjonas25@gmail.com>
2025-12-04 20:07:59 +0000

    Refactor

--- y-25/src/main/kotlin/io/github/aj8gh/aok/y25/d4/Day4.kt
+++ y-25/src/main/kotlin/io/github/aj8gh/aok/y25/d4/Day4.kt
@@ -4,34 +4,33 @@ import io.github.aj8gh.aok.util.LEVEL_1
 import io.github.aj8gh.aok.util.LEVEL_2
 
 private const val ROLL = '@'
-private const val EMPTY = '.'
 private const val THRESHOLD = 4
 
-fun part1(input: List<String>) = findAccessible(input, LEVEL_1)
+fun part1(input: List<String>) = solve(input, LEVEL_1)
 
-fun part2(input: List<String>) = findAccessible(input, LEVEL_2)
+fun part2(input: List<String>) = solve(input, LEVEL_2)
 
-private fun findAccessible(input: List<String>, level: Int): Int {
-  val grid = input.map { it.toMutableList() }
-  var total = 0
-  for (i in grid.indices) {
-    for (j in grid[i].indices) {
-      total += score(Pair(i, j), grid, level)
+private fun solve(input: List<String>, level: Int): Int {
+  val grid = mutableMapOf<Pair<Int, Int>, Boolean>()
+  for (i in input.indices) {
+    for (j in input[i].indices) {
+      if (input[i][j] == ROLL) {
+        grid[Pair(i, j)] = true
+      }
     }
   }
-  return total
+
+  return grid.keys.sumOf { score(it, grid, level) }
 }
 
 private fun score(
   point: Pair<Int, Int>,
-  input: List<MutableList<Char>>,
-  level: Int
+  grid: MutableMap<Pair<Int, Int>, Boolean>,
+  level: Int,
 ): Int {
-  if (isOffgrid(point, input) || input[point.first][point.second] != ROLL) {
-    return 0
-  }
+  if (!(grid[point] ?: false)) return 0
 
-  val points = listOf(
+  val points = setOf(
     Pair(point.first + 1, point.second),
     Pair(point.first + 1, point.second + 1),
     Pair(point.first + 1, point.second - 1),
@@ -44,21 +43,12 @@ private fun score(
 
   var total = 0
   for (p in points) {
-    if (isOffgrid(p, input)) continue
-    if (input[p.first][p.second] == ROLL && ++total >= THRESHOLD) return 0
+    if (grid[p] ?: false && ++total >= THRESHOLD) return 0
   }
 
   return if (level == LEVEL_1) 1
   else {
-    input[point.first][point.second] = EMPTY
-    points.sumOf { score(it, input, level) } + 1
+    grid[point] = false
+    points.sumOf { score(it, grid, level) } + 1
   }
 }
-
-private fun isOffgrid(
-  point: Pair<Int, Int>,
-  input: List<List<Char>>
-) = point.first < 0
-    || point.first > input.lastIndex
-    || point.second < 0
-    || point.second > input[point.first].lastIndex
