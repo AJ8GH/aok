1181e94aba6433870256c324fb053ff8ea2a5ddb
Adam Jonas <adamjonas25@gmail.com>
2025-12-06 22:12:44 +0000

    Refactor

--- y-25/src/main/kotlin/io/github/aj8gh/aok/y25/d6/Day6.kt
+++ y-25/src/main/kotlin/io/github/aj8gh/aok/y25/d6/Day6.kt
@@ -1,60 +1,46 @@
 package io.github.aj8gh.aok.y25.d6
 
-private val spacesRgx = Regex("\\s+")
-private val opChunkRgx = Regex("[+*]\\s+")
+private const val ADD = "+"
+
+private val sRgx = Regex("\\s+")
+private val opRgx = Regex("[+*]\\s+")
 
 fun part1(input: List<String>): Long {
-  val nums = input
-    .slice(0..<input.lastIndex)
-    .map {
-      it.split(spacesRgx)
-        .filter { s -> s.isNotBlank() }
-        .map { s -> s.toLong() }
-    }
-
-  val operators = input.last().split(spacesRgx)
-
-  var total = 0L
-  for (i in nums.first().indices) {
-    val operands = mutableListOf<Long>()
-    for (l in nums) operands.add(l[i])
-
-    total += calculate(operands, operators[i])
-  }
-  return total
+  val nums = input.slice(0..<input.lastIndex)
+    .map { it.split(sRgx).filter { s -> s.isNotBlank() }.map { s -> s.toLong() } }
+  val operators = input.last().split(sRgx)
+  return nums.first().indices.mapIndexed { i, _ ->
+    calculate(nums.map { it[i] }, operators[i])
+  }.sum()
 }
 
 fun part2(input: List<String>): Long {
-  val operators = opChunkRgx.findAll(input.last()).map { it.value }.toList()
-
+  val ops = opRgx.findAll(input.last()).map { it.value }.toList()
   var total = 0L
-  var j = 0
-  for (i in operators.indices) {
-    val opChunk = operators[i]
-    var endNumI =
-      if (i == operators.lastIndex) opChunk.lastIndex + j
-      else opChunk.lastIndex + j - 1
-
+  var i = 0
+  for (j in ops.indices) {
+    val op = ops[j]
+    val rightIndex = (op.lastIndex + i).let { if (j == ops.lastIndex) it else it - 1 }
     val nums = mutableListOf<Long>()
-    for (k in endNumI downTo j) {
-      var n = 0L
-      for (s in input.slice(0..<input.lastIndex)) {
-        val c = s[k]
-        if (c == ' ') {
-          continue
+    for (k in rightIndex downTo i) {
+      var num = 0L
+      for (line in input.slice(0..<input.lastIndex)) {
+        line[k].takeIf { it != ' ' }?.let {
+          num += num * 9L + it.digitToInt()
         }
-        n *= 10L
-        n += c.digitToInt()
       }
-      nums.add(n)
+      nums.add(num)
     }
-    j += opChunk.length
-    total += calculate(nums, opChunk.first().toString())
+    i += op.length
+    total += calculate(nums, op.first().toString())
   }
   return total
 }
 
-private fun calculate(operands: List<Long>, operator: String) = operands
-  .reduce { a, b ->
-    if (operator == "+") a + b else a * b
+private fun calculate(operands: List<Long>, operator: String) =
+  operands.reduce { a, b ->
+    when (operator) {
+      ADD -> a + b
+      else -> a * b
+    }
   }
